a258cfeaafe19e0b0db4c2fd2fe401bb50632580
Larry Volz <imaginologist@gmail.com>
2020-11-18 21:59:30 -0500

    graphics added

--- chessLogic.js
+++ chessLogic.js
@@ -1,13 +1,41 @@
 
+
+const wr = { utfCode: '\u2656',
+            color: 'white'}
+const wn = {utfCode: '\u2658',
+            color: 'white' }
+const wb = {utfCode: '\u2657',
+            color: 'white' }
+const wq = {utfCode: '\u2655',
+            color: 'white' }
+const wk = {utfCode: '\u2654',
+            color: 'white'}
+const wp = {utfCode: '\u2659',
+            color: 'white'}
+
+const br = { utfCode: '\u265C',
+            color: 'black'}
+const bn = {utfCode: '\u265E',
+            color: 'black' }
+const bb = {utfCode: '\u265D',
+            color: 'black' }
+const bq = {utfCode: '\u265B',
+            color: 'black' }
+const bk = {utfCode: '\u265A',
+            color: 'black'}
+const bp = {utfCode: '\u265F',
+            color: 'black'}  
+            
+            
 const rankAndFile = [
-    ['wr','wn','wb','wq','wk','wb','wn','wr'],
-    ['wp','wp','wp','wp','wp','wp','wp','wp'],
+    [wr,wn,wb,wq,wk,wb,wn,wr],
+    [wp,wp,wp,wp,wp,wp,wp,wp],
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
-    ['bp','bp','bp','bp','bp','bp','bp','bp'],
-    ['br','bn','bb','bq','bk','bb','bn','br'],
+    [bp,bp,bp,bp,bp,bp,bp,bp],
+    [br,bn,bb,bq,bk,bb,bn,br],
 ];
 
 const letters = "abcdefgh";
@@ -25,7 +53,7 @@ function drawBoard () {
             // console.log(`id: ${idCode}, rank: ${rank}, file: ${fil}`);
 
             //   console.log(`rankAndFile: ${rankAndFile[fil][rank]}`);
-              currentPiece = rankAndFile[rank][fil];
+              currentPiece = rankAndFile[rank][fil].utfCode;
               board = document.querySelector(idCode);
               board.innerHTML = (currentPiece) ? currentPiece : '&nbsp';
             
